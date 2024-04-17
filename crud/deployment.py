a9dd402e422bc84f391ff454f7ece58050a33916
iqbal-developer <129623820+iqbal-developer@users.noreply.github.com>
2024-04-18 01:45:22 +0300

    Update deployment.py to update allowed frontend

--- crud/deployment.py
+++ crud/deployment.py
@@ -20,7 +20,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-   "https://wonderful-cliff-01e8deb0f.5.azurestaticapps.net"
+   "https://polite-mushroom-049edf40f.5.azurestaticapps.net"
 ]
 
 
@@ -44,35 +44,6 @@ DATABASES = {
     }
 }
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'class': 'django.utils.log.AdminEmailHandler',
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['mail_admins'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#     },
-# }
-
-
-
-# ADMINS = [("Nick", "YOURMAIL.com")]
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# DEFAULT_FROM_EMAIL = 'default from email'
 
 
 
